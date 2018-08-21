6cadfd6b4f4ad64a3375b750ac50d0cb007f35b7
Wenish <Jonas.Voland@swisscom.com>
2018-08-21 18:39:08 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/dist/vuetify.min.css'
+import App from '@/components/GoogleYoutube.vue'
+//import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
