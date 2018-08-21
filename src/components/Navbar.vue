f0d4af9beae3e77eb0908dda305d29313eebfc61
Wenish <jonas.voland@bluewin.ch>
2018-08-21 23:31:18 +0200

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,4 @@
 <template>
-  <v-app id="inspire" dark>
     <v-navigation-drawer v-model="drawer" fixed clipped app>
       <v-list dense>
         <v-list-tile v-for="item in items" :key="item.text" @click="">
@@ -35,51 +34,6 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar color="red" dense fixed clipped-left app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-icon class="mx-3">fab fa-youtube</v-icon>
-      <v-toolbar-title class="mr-5 align-center">
-        <span class="title">Youtube</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-layout row align-center style="max-width: 650px">
-        <v-text-field
-          :append-icon-cb="() => {}"
-          placeholder="Search..."
-          single-line
-          append-icon="search"
-          color="white"
-          hide-details
-        ></v-text-field>
-      </v-layout>
-    </v-toolbar>
-    <v-content>
-      <v-container fill-height>
-        <v-layout justify-center align-center>
-          <v-flex shrink>
-            <v-tooltip right>
-              <v-btn
-                slot="activator"
-                :href="source"
-                icon
-                large
-                target="_blank"
-              >
-                <v-icon large>code</v-icon>
-              </v-btn>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/YeRKwQ" target="_blank">
-                <v-icon large>mdi-codepen</v-icon>
-              </v-btn>
-              <span>Codepen</span>
-            </v-tooltip>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
 </template>
 
 <script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-toolbar color="red" dense fixed clipped-left app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-icon class="mx-3">fab fa-youtube</v-icon>
+      <v-toolbar-title class="mr-5 align-center">
+        <span class="title">Youtube</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-layout row align-center style="max-width: 650px">
+        <v-text-field
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          single-line
+          append-icon="search"
+          color="white"
+          hide-details
+        ></v-text-field>
+      </v-layout>
+    </v-toolbar>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: true
+    })
+  }
+</script>
