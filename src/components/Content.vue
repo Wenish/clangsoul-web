98cae410286ccb1340c9ad7180868b896743c258
Wenish <Jonas.Voland@swisscom.com>
2018-08-23 17:33:24 +0200

    update

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,5 +1,6 @@
 <template>
     <v-content>
+        <v-parallax dark src="/img/assets/nightBlueBackground.jpg" height="200"></v-parallax>
         <router-view/>
     </v-content>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,18 @@
-<template>
-  <v-container fill-height>
-      <div class="home">
-        <img class="img" alt="Clangsoul logo" src="../assets/logoClangsoulBlack.png">
-        just vibe
-      </div>
+<template fill-height>
+  <v-container>
+      <Releases/>
+      <div>just vibe</div>
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Releases from '@/components/Releases'
 
 export default {
   name: 'home',
   components: {
+    Releases
   }
 }
 </script>
-<style scoped>
-.img {
-    margin: auto;
-    width: 100%;
-}
-</style>
