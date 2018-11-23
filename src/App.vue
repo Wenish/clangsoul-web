6d621f543838ee22d65dd693d2ccf718fa5e300e
Wenish <jonas.voland@bluewin.ch>
2018-11-23 14:27:34 +0100

    update colors

--- src/App.vue
+++ src/App.vue
@@ -18,3 +18,25 @@ export default {
   }
 }
 </script>
+
+<style lang="css">
+.application.theme--dark {
+    background: #001011;
+}
+
+.flex:hover {
+    filter: drop-shadow(0 0 30px #333);
+    transition-duration: 0.2s;
+    cursor: default;
+}
+
+
+.application .theme--dark.v-toolbar, .theme--dark .v-toolbar {
+    background-color: #080808;
+    filter: drop-shadow(0 0 20px #333); 
+}
+
+.application .theme--dark.v-card, .theme--dark .v-card {
+    background-color: #192B35;
+}
+</style>
